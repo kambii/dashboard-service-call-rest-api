f53ec4688634c96cd6fa3bb405684c2a844e812c
kambii <kambinho123@hotmail.com>
2022-11-13 17:27:00 +0100

    ready

--- src/main/java/com/example/dashboardservicecallrestapi/model/Todo.java
+++ src/main/java/com/example/dashboardservicecallrestapi/model/Todo.java
@@ -1,5 +1,8 @@
 package com.example.dashboardservicecallrestapi.model;
 
 
-public class Todo {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+
+public record Todo(@Id Integer id, Integer userId, String title, boolean completed, @Version Integer version) {
 }
