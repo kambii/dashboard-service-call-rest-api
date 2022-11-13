34d508d0022e43b86c97fa8975445dfab9ea74bf
kambii <kambinho123@hotmail.com>
2022-11-13 17:23:59 +0100

    ready

---
+++ src/main/java/com/example/dashboardservicecallrestapi/model/Todo.java
@@ -0,0 +1,5 @@
+package com.example.dashboardservicecallrestapi.model;
+
+
+public class Todo {
+}
---
+++ src/test/java/com/example/dashboardservicecallrestapi/DashboardServiceCallRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dashboardservicecallrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardServiceCallRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
